8c52c1b91359ea208e3dbe7a46036df3ef58967c
Hyeryung <hyeryungjoo@gmail.com>
2022-07-06 23:41:25 +0900

    220706

---
+++ BOJ/BOJ_1916_푸는 중.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BOJ_1916 {
+
+	static int n, m, start, end, ans[];
+	static ArrayList<Edge>[] list;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		n = Integer.parseInt(br.readLine());
+		m = Integer.parseInt(br.readLine());
+
+		list = new ArrayList[n + 1];
+		for (int i = 1; i <= n; i++)
+			list[i] = new ArrayList<>();
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+			list[from].add(new Edge(to, weight));
+		}
+
+		st = new StringTokenizer(br.readLine());
+		start = Integer.parseInt(st.nextToken());
+		end = Integer.parseInt(st.nextToken());
+
+		dijkstra();
+
+		bw.write(ans[end] + "");
+		bw.flush();
+		bw.close();
+	}
+
+	public static void dijkstra() {
+
+		ans = new int[n + 1];
+		Arrays.fill(ans, Integer.MAX_VALUE);
+		ans[start] = 0;
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		pq.add(new Edge(start, 0));
+
+		while (!pq.isEmpty()) {
+
+			Edge edge = pq.poll();
+
+			int curr = edge.to;
+
+			for (Edge out : list[curr]) {
+				if (ans[out.to] > ans[curr] + out.weight) {
+					ans[out.to] = ans[curr] + out.weight;
+					pq.add(new Edge(out.to, ans[out.to]));
+				}
+			}
+
+		}
+	}
+
+	public static class Edge implements Comparable<Edge> {
+		int to, weight;
+
+		public Edge(int to, int weight) {
+			super();
+			this.to = to;
+			this.weight = weight;
+		}
+
+		@Override
+		public int compareTo(Edge o) {
+			return this.weight - o.weight;
+		}
+	}
+}
